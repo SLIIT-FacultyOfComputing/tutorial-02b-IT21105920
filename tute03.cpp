ee9a353c35b474e01e27cc5edca79df68feb8be8
it21105920 <87320518+IT21105920@users.noreply.github.com>
2021-07-22 03:55:43 +0000

    Completed

--- tute03.cpp
+++ tute03.cpp
@@ -24,30 +24,6 @@ int main()
 }
 */
 
-/*
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    int no , r;	
-	long fac;
-	
-	cout << "Enter a Number : ";
-	cin >> no;
-	
-	fac = 1;
- 
-    for(r = no; r >= 1; r--) 
-    {
-        fac = fac * r;
-    }  
-    
-    cout << "Factorial of " << no << " is " << fac << endl;
-    
-	return 0;
-}
-*/
 
 #include<iostream>
 using namespace std;
